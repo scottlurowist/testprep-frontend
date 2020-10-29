5e71af7b1acbc76fa85caf922a6265662406d675
scottlurowist <scottlurowist@gmail.com>
2020-10-29 12:11:04 -0400

    MOST OF THE CONTROLS ARE IN PLACE FOR EDIT TEST
    Most of the controls are in place, but not working. I am limiting the
    tests to 4 choices. THe back and next question buttons are in place but
    not working at the moment. There are also Add and Delete question
    buttons but are not working at the moment.
    
    The text fields are bound to state.
    
    The select best and select all that apply radio buttons force the
    is correct check fields to render as radio buttons or check boxes. This
    is actually working at the moment.
    
    I am making a commit so that I don't lose any work.

--- src/views/EditTest/EditTestView.jsx
+++ src/views/EditTest/EditTestView.jsx
@@ -10,10 +10,10 @@
 
 import React, { Fragment } from 'react';
 import { withRouter } from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 import TestprepDataModel from './../../api/data-model';
-import questionTypeEnum from '../../lib/questionTypeEnum';
 
 
 
@@ -26,15 +26,24 @@ class EditTestView extends React.Component {
         super(props);
 
         this.state = {
-            loaded: false,
-            currentQuestion: null,
-            checkAnswerButtonDisabled: false,
-            nextQuestionButtonDisabled: true,
+            testName: '',
+            testDescription: '',
+            currentQuestion: '',
+            selectedOption: '',
+            currentFirstChoice: '',
+            currentFirstChoiceIsCorrect: '',
+            currentSecondChoice: '',
+            currentSecondChoiceIsCorrect: '',
+            currentThirdChoice: '',
+            currentThirdChoiceIsCorrect: '',
+            currentFourthChoice: '',
+            currentFourthChoiceIsCorrect: '',
+            choiceType: 'radio',
             user: props.user
         };
 
         this.test = null;
-        this.currentQuestionIndex = -1;
+        this.currentQuestionIndex = 0;
         this.numberOfQuestions = -1; 
         this.msgAlert =  props.msgAlert;
         this.dataModel = new TestprepDataModel();
@@ -52,22 +61,98 @@ class EditTestView extends React.Component {
             const response = await this.dataModel
                                        .getATest(this.state.user.token, testId);
 
-            this.test = response.data.test; 
-            // this.currentQuestionIndex = 0;
-            // this.numberOfQuestions = this.test.questions.length; 
-            // this.answersToTheQuestion = null;
+            this.test = response.data.test;
+            this.numberOfQuestions = this.test.questions.length;
 
-            // this.setState({
-            //     loaded: true,
-            //     currentQuestion: this.test.questions[this.currentQuestionIndex],
-            // }); 
+            this.setState({
+                 testName: this.test.name,
+                 testDescription: this.test.description,
+                 currentQuestion: this.test.questions[0].text,
+                 currentFirstChoice: this.test.questions[0].choices[0].text,
+                 currentSecondChoice: this.test.questions[0].choices[1].text,
+                //  currentThirdChoice: this.test.questions[0].choices[2].text,
+                //  currentFourthChoice: this.test.questions[0].choices[3].text,
+                 currentFirstChoiceIsCorrect: this.test.questions[0].choices[0].isAnswer,
+                 currentSecondChoiceIsCorrect: this.test.questions[0].choices[1].isAnswer,
+                //  currentThirdChoiceIsCorrect: this.test.questions[0].choices[2].isAnswer,
+                //  currentFourthChoiceIsCorrect: this.test.questions[0].choices[3].isAnswer,
+                 selectedOption: this.test.questions[0].type,
+                 choiceType: 'radio'
+            });
         }
         catch(err) {
-
             this.msgAlert(
                 {heading: 'Error', message: err.message, variant: 'danger'});
         }
     };
+
+
+    onTestNameChanged = event => {
+        this.setState({ testName: event.target.value });
+    };
+
+
+    onTestDescriptionChanged = event => {
+        this.setState({ testDescription: event.target.value });
+    }
+
+
+    onCurrentQuestionChanged = event => {
+        this.setState({ currentQuestion: event.target.value });
+    }
+
+
+    onSubmitHandler = event => {
+        event.preventDefault();
+    };
+
+
+    handleOptionChange = event => {
+        let choiceType
+
+        if (event.target.value === 'selectBest') {
+          choiceType = 'radio';    
+        }
+        else {
+            choiceType = 'checkbox';
+        }
+
+        this.setState({
+          selectedOption: event.target.value,
+          choiceType: choiceType
+        });
+    };
+
+
+    onCurrentFirstChoiceChanged = event => {
+
+        this.setState({
+          currentFirstChoice: event.target.value,
+        });
+    };
+
+
+    onCurrentSecondChoiceChanged = event => {
+        this.setState({
+          currentSecondChoice: event.target.value
+        });
+    };
+
+
+    onCurrentThirdChoiceChanged = event => {
+        this.setState({
+          currentThirdChoice: event.target.value
+        });
+    };
+
+
+    onCurrentFourthChoiceChanged = event => {
+        this.setState({
+            currentFourthChoice: event.target.value,
+        });
+    };
+ 
+
 
     // A React.js lifecyle method that is invoked whenever state changes and
     // renders the component.
@@ -76,11 +161,93 @@ class EditTestView extends React.Component {
         return (
             <Fragment>
                 <h3>Edit A Test</h3>
-                {/* { this.state.loaded &&
-                  <div>
-                    <h2>{this.test.name}</h2>
-                  </div>
-                } */}
+                <Form onSubmit={event => this.onSubmitHandler(event)}>
+                    <Form.Group controlId="formTestName">
+                        <Form.Label>Test Name</Form.Label>
+                        <Form.Control type="text" placeholder="test name" 
+                                      value={ this.state.testName }
+                                      onChange={event => this.onTestNameChanged(event) } />
+                    </Form.Group>
+                    <Form.Group controlId="formCurrentQuestion">
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control type="text" placeholder="description"
+                                      value={this.state.testDescription}
+                                      onChange={event => this.onTestDescriptionChanged(event) }  />
+                    </Form.Group>
+                    <Form.Group controlId="formCurrentQuestion">
+                        <Button variant="primary" type="button">
+                            &lt;&lt;
+                        </Button>
+                        <Button variant="primary" type="button">
+                            &gt;&gt;
+                        </Button>
+                        <Button variant="primary" type="button">
+                            Add Question;
+                        </Button> 
+                        <Button variant="primary" type="button">
+                            Delete Question;
+                        </Button>                                                                           
+                    </Form.Group>                         
+                    <Form.Group controlId="formCurrentQuestion">
+                        <Form.Label>Current Question</Form.Label>
+                        <Form.Control type="text" placeholder="current question"
+                                      value={this.state.currentQuestion}
+                                      onChange={event => this.onCurrentQuestionChanged(event) }  />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox">
+                        <Form.Check id='select-best' inline type="radio" 
+                                    value='selectBest' 
+                                    onChange={event => this.handleOptionChange(event)}
+                                    name='questionType'
+                                    label="select best answer" />
+                        <Form.Check id='select-all' inline type="radio"
+                                    value='selectAllThatApply'
+                                    onChange={event => this.handleOptionChange(event)}
+                                    name="questionType"
+                                    label="select all that apply" >
+                        </Form.Check>
+                    </Form.Group>                    
+                    <Form.Label>Choices</Form.Label>
+                    <Form.Group controlId="formBasicCheckbox">
+                        <Form.Control type="text" placeholder="first choice"
+                                      className='w-50'
+                                      value={this.state.currentFirstChoice}
+                                      onChange={event => this.onCurrentFirstChoiceChanged(event) } />
+                        <Form.Check id='isCorrect-1' inline type={this.state.choiceType}
+                                    value=''
+                                    name='choice' label='Is Correct?' />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox">
+                        <Form.Control type="text" placeholder="second choice"
+                                      className='w-50'
+                                      value={this.state.currentSecondChoice}
+                                      onChange={event => this.onCurrentSecondChoiceChanged(event) }  />
+                        <Form.Check id='isCorrect-2' inline type={this.state.choiceType}
+                                    value=''
+                                    name='choice' label='Is Correct?' />              
+                    </Form.Group> 
+                    <Form.Group controlId="formBasicCheckbox">
+                        <Form.Control type="text" placeholder="third choice"
+                                      className='w-50'
+                                      value={this.state.currentThirdChoice}
+                                      onChange={event => this.onCurrentThirdChoiceChanged(event) }  />
+                        <Form.Check id='isCorrect-3' inline type={this.state.choiceType}
+                                    value=''
+                                    name='choice' label='Is Correct?' />                                        
+                    </Form.Group>      
+                    <Form.Group controlId="formBasicCheckbox" className='w-75'>
+                        <Form.Control type="text" placeholder="fourth choice"
+                                      value={this.state.currentFourthChoice}
+                                      onChange={event => this.onCurrentFourthChoiceChanged(event) }  />
+                        <Form.Check id='isCorrect-4' inline type={this.state.choiceType}
+                                    value=''
+                                    name='choice' label='Is Correct?' />                                        
+                    </Form.Group>                          
+
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
             </Fragment>
         );
     };
